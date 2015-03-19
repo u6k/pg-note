50dcc65e2584ff02b239b6d837a54456ec05db20
Naono Yuichi <u6k.apps@gmail.com>
2015-03-19 20:56:12 +0900

    listコマンドの空クラスを作成しました。

--- pg-note/src/main/java/jp/gr/java_conf/u6k/pg_note/PgNoteApplication.java
+++ pg-note/src/main/java/jp/gr/java_conf/u6k/pg_note/PgNoteApplication.java
@@ -2,12 +2,19 @@
 package jp.gr.java_conf.u6k.pg_note;
 
 import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+import jp.gr.java_conf.u6k.pg_note.command.ListCommand;
 
 public class PgNoteApplication extends Application<PgNoteConfiguration> {
 
     public static void main(String[] args) throws Exception {
         new PgNoteApplication().run(args);
+    }
+
+    @Override
+    public void initialize(Bootstrap<PgNoteConfiguration> bootstrap) {
+        bootstrap.addCommand(new ListCommand());
     }
 
     @Override
